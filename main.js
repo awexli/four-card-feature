b265173b801c670e0c8d2cb8de3cd7059daf0395
awexli <awexli@mail.sfsu.edu>
2020-05-06 19:00:41 -0700

    add first iteration of header format

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    image: './images/icon-calculator.svg',
+    header: {
+      top: 'Reliable, efficient delivery',
+      bot: 'Powered by Technology',
+      sub:
+        'Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful',
+    },
+  },
+});
