b1f3032c0834822f0b4ef8e7ef703b1b9aad94ce
awexli <awexli@mail.sfsu.edu>
2020-05-06 19:44:53 -0700

    add first iteration of card format

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const app = new Vue({
   el: '#app',
   data: {
-    image: './images/icon-calculator.svg',
     header: {
       top: 'Reliable, efficient delivery',
       bot: 'Powered by Technology',
       sub:
         'Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful',
+    },
+    card: {
+      left: {
+        title: 'Supervisor',
+        sub: 'Monitors activity to identify project roadblocks',
+        icon: './images/icon-supervisor.svg',
+      },
     },
   },
 });
